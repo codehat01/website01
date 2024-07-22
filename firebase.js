2ab4709ae9dbd6d997008896820ba0ef2737c446
codehat01 <144668556+codehat01@users.noreply.github.com>
2024-07-22 10:26:13 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCH5Sv6SKrJr2nphtWGsiSLXUWpzcUkGuA",
+    authDomain: "acn-2024-9779f.firebaseapp.com",
+    projectId: "acn-2024-9779f",
+    storageBucket: "acn-2024-9779f.appspot.com",
+    messagingSenderId: "1035635010826",
+    appId: "1:1035635010826:web:f8667121f4463aeed99ef3",
+    measurementId: "G-WMZPGLM1XM"
+  };
+  firebase.initializeApp(firebaseConfig);

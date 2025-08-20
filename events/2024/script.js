3cffc8f9dfed9eba1176800d37bee640563ab8a0
sathvikvalivety <sathvikvalivety@gmail.com>
2025-08-20 20:19:48 +0530

    changes

--- events/2024/script.js
+++ events/2024/script.js
@@ -15,15 +15,27 @@ document.addEventListener('DOMContentLoaded', function() {
         cardsContainer.style.scrollBehavior = 'smooth';
     }
 
-    // Change navbar style on scroll
-    const nav = document.querySelector("nav");
-    if (nav) {
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > 50) { 
-                nav.classList.add("scrolled");
-            } else {
-                nav.classList.remove("scrolled");
-            }
-        });
+
+
+    // Loading screen functionality
+    const loadingScreen = document.getElementById("loading-screen");
+    const mainContent = document.getElementById("main-content");
+    if (loadingScreen && mainContent) {
+        setTimeout(function() {
+            // Hide the loading screen
+            loadingScreen.style.display = "none";
+            
+            // Show the main content
+            mainContent.style.display = "block";
+        }, 3000); // 3000 milliseconds = 3 seconds
+    } else {
+        console.error("Loading screen or main content element not found");
     }
 });
+
+// Function to show text
+function showText(text) {
+    document.getElementById('card-text1').innerText = text;
+}
+
+
